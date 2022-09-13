4d5b04f59c08cab7bb7eb6e86d7d74049acd4671
Paddy-31 <pradnumore11@gmail.com>
2022-09-13 11:30:12 +0530

    new file:   Egg.py 	new file:   Hello.py 	new file:   Pong.py 	new file:   canon.py 	new file:   main.py 	new file:   road.gif 	new file:   row.py 	new file:   space.png 	new file:   spin.py 	new file:   tempCodeRunnerFile.py 	new file:   tictaktoe.py 	new file:   ufo.png 	new file:   workspace.code-workspace
    first commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import pygame
+
+pygame.init()
+
+screen = pygame.display.set_mode((800,600))
+
+pygame.display.set_caption("Space Invaders")
+icon = pygame.image.load('ufo.png')
+pygame.display.set_icon(icon)
+
+playerImg = pygame.image.load('space.png')
+playerX = 370
+playerY = 480
+playerX_change = 0
+
+
+
+
+def player(x,y):
+        screen.blit(playerImg, (x, y))
+
+
+running = True 
+while running:
+        
+    screen.fill((0,0,0))
+    
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+    
+        if event.type == pygame.KEYDOWN:
+            print("keystroke is pressed")
+            if event.key == pygame.K_LEFT:
+                playerX_change = -0.3
+            if event.key == pygame.K_RIGHT:
+                playerY_change = 5
+
+        if event.type == pygame.KEYUP:
+            if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+                playerY_change = 0
+        
+        playerX += playerX_change
+        player(playerX,playerY)
+        pygame.display.update()
+            
